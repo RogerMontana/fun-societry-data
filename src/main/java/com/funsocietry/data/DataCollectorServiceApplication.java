41cdc000cc40ab8b98f87c0fb06b9783b401cea3
Artem K <artem.karpov@yahoo.com>
2018-09-09 23:59:45 +0300

    Initial commit

---
+++ src/main/java/com/funsocietry/data/DataCollectorServiceApplication.java
@@ -0,0 +1,29 @@
+package com.funsocietry.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Calendar;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@RestController
+public class DataCollectorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataCollectorServiceApplication.class, args);
+	}
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello Spring Boot, I'm on App Engine!!!";
+    }
+    @GetMapping("/time")
+    public String time() {
+        return Calendar.getInstance().getTime().toString();
+    }
+}
