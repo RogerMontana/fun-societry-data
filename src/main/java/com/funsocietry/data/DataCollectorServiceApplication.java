2d35fc187e0e13b9f722c8c09b0b1c386705a5cb
Artem K <artem.karpov@yahoo.com>
2018-09-10 01:06:26 +0300

    remove version

--- src/main/java/com/funsocietry/data/DataCollectorServiceApplication.java
+++ src/main/java/com/funsocietry/data/DataCollectorServiceApplication.java
@@ -1,16 +1,13 @@
 package com.funsocietry.data;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Calendar;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @RestController
 public class DataCollectorServiceApplication {
 
@@ -18,15 +15,15 @@ public class DataCollectorServiceApplication {
 		SpringApplication.run(DataCollectorServiceApplication.class, args);
 	}
 
-    @GetMapping("/")
+    @RequestMapping("/")
     public String hello() {
         return "Hello Spring Boot, I'm on App Engine!!!";
     }
-    @GetMapping("/time")
+    @RequestMapping("/time")
     public String time() {
         return Calendar.getInstance().getTime().toString();
     }
-    @GetMapping("/_ah/health")
+    @RequestMapping("/_ah/health")
     public String healthy() {
         return "Still surviving.";
     }
