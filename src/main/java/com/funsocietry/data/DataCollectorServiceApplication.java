140b6a59c0328851fbdc8e5baf12b3b7bbcf2419
Artem K <artem.karpov@yahoo.com>
2018-09-12 00:23:58 +0300

    remove version

--- src/main/java/com/funsocietry/data/DataCollectorServiceApplication.java
+++ src/main/java/com/funsocietry/data/DataCollectorServiceApplication.java
@@ -1,13 +1,7 @@
 package com.funsocietry.data;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Calendar;
 
 @SpringBootApplication
 public class DataCollectorServiceApplication {
---
+++ src/main/java/com/funsocietry/data/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.funsocietry.data;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * Created by artem on 9/12/18.
+ */
+public class ServletInitializer extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DataCollectorServiceApplication.class);
+    }
+}
