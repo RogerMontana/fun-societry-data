2a3febeb726448b94d45cb96b2a9439485c0c060
akarpov <akarpov@playtika.com>
2018-10-03 13:04:40 +0300

    fix

--- src/main/java/com/funsocietry/data/StaticUIController.java
+++ src/main/java/com/funsocietry/data/StaticUIController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @AllArgsConstructor
 public class StaticUIController {
 
-    @GetMapping(value = "/")
+    @GetMapping(value = "/index")
     public String index() {
         return "index";
     }
