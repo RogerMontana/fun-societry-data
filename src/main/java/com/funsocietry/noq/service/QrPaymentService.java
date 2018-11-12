60e10a80fbb68bae4cb5e50c97a84c701c379bd1
Artem K <artem.karpov@yahoo.com>
2018-11-12 23:56:41 +0200

    trying add some ui

--- src/main/java/com/funsocietry/noq/service/QrPaymentService.java
+++ src/main/java/com/funsocietry/noq/service/QrPaymentService.java
@@ -2,6 +2,7 @@ package com.funsocietry.noq.service;
 
 import com.funsocietry.noq.domain.QrCode;
 import lombok.AllArgsConstructor;
+import org.springframework.core.io.ClassPathResource;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -10,15 +11,18 @@ import java.io.IOException;
 @AllArgsConstructor
 public class QrPaymentService {
 
+
     public QrCode getPaymentCode() throws IOException {
         QrCode qr = new QrCode();
-        qr.setEncodedQrCode(getBytes(new File("src/main/resources/static/qr/example_qr.png")));
+
+        ClassPathResource imgFile = new ClassPathResource("static/qr/example_qr.png");
+        qr.setEncodedQrCode(getBytes(imgFile.getFile());
         return qr;
     }
-    
+
     private byte[] getBytes(File file) throws IOException {
         FileInputStream fileInputStreamReader = new FileInputStream(file);
-        byte[] bytes = new byte[(int)file.length()];
+        byte[] bytes = new byte[(int) file.length()];
         fileInputStreamReader.read(bytes);
         return bytes;
     }
